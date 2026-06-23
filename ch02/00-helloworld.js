6a9f28a6b200c91f7866accbabc2ee22d782f957
Jeff Yankson <jjyankson19@gmail.com>
2026-06-23 17:54:29 +0000

    Serve HTML instead of plain text

--- ch02/00-helloworld.js
+++ ch02/00-helloworld.js
@@ -4,11 +4,11 @@ const http = require('node:http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Hello World!');
+  res.writeHead(200, { 'content-type': 'text/html' });
+  res.end('<p> Hello World! </p>');
 });
 
-// Start server.
+// Start server and navigate to http://localhost:3000
 server.listen(PORT, () => {
   console.log(`Server started on Port: ${PORT}` + 'Press Ctrl + c to cancel.');
 });

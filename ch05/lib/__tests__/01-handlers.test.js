13b47e1c529116e54a82fe4c0f1f27bf5bf37f9d
Jeff Yankson <jjyankson19@gmail.com>
2026-06-25 13:02:43 +0000

    Tests: about page renders fortune

--- ch05/lib/__tests__/01-handlers.test.js
+++ ch05/lib/__tests__/01-handlers.test.js
@@ -6,3 +6,12 @@ test('renders home page', () => {
   handlers.home(req, res);
   expect(res.render.mock.calls[0][0]).toBe('home');
 });
+
+test('about page renders with fortune', () => {
+  const req = {};
+  const res = { render: jest.fn() };
+  handlers.about(req, res);
+  expect(res.render.mock.calls.length).toBe(1);
+  expect(res.render.mock.calls[0][0]).toBe('about');
+});
+
